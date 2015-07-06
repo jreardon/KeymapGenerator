f86939cceba3a1a1b3cdb0099ae6314ca03d576a
jreardon <jason.reardon@architectinginnovation.com>
2015-07-06 10:49:46 -0700

    Added Action object

---
+++ KeymapGenerator/KeymapGenerator/Models/Action.cs
@@ -0,0 +1,11 @@
+﻿using KeymapGenerator.DataTypes;
+
+namespace KeymapGenerator.Models
+{
+    public class Action
+    {
+        public KeymapType ActionType { get; set; }
+        public int ActionNumber { get; set; }
+        public int RefLayerNumber { get; set; }
+    }
+}
